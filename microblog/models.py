630c3078df8d0aad235d7891b7f70c04b1037305
Sergey McArov <naurion@yandex.ru>
2023-04-27 11:55:29 +0300

    First

---
+++ microblog/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from core.db import Base
+
+
+class Post(Base):
+    __tablename__ = "microblog_posts"
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    title = Column(String)
+    text = Column(String(350))
+    date = Column(DateTime)
+    user = Column(Integer, ForeignKey("users.id"))
+    user_id = relationship("User")
+
